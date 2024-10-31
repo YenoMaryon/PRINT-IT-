db0e2e19a591be0c1b4e94f501718e2e19ba124c
YenoMaryon <maryon.yeno@gmail.com>
2024-10-31 14:03:44 +0100

    integration of the dots of the caroussel and different functions to create, modify and update them

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,60 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+let index = 0
+
+const leftButton = document.querySelector("#left")
+leftButton.addEventListener("click" , function(){
+	if (index === 0){
+		index = 3
+	} else {
+		index--
+	}
+	showImage()
+})
+
+const rightButton = document.querySelector("#right")
+rightButton.addEventListener("click" , function(){
+	if (index === 3){
+		index = 0
+	} else {
+		index++
+	}
+	showImage()
+})
+showImage()
+createDots()
+
+function showImage (){
+	const currentSlide = slides[index]
+	const bannerImg = document.querySelector(".banner-img")
+	bannerImg.src = "./assets/images/slideshow/" + currentSlide.image
+	const bannerText = document.querySelector("#banner p")
+	bannerText.innerHTML = currentSlide.tagLine
+
+	updateDots()
+}
+
+function createDots(){
+	 const dotsContainer = document.querySelector(".dots")
+	for(let i = 0; i < slides.length; i++){
+		const dot = document.createElement("div")
+		dot.classList.add("dot")
+		dot.addEventListener("click", () => {
+			index = i
+			showImage()
+		})
+		dotsContainer.append(dot)
+	}
+}
+
+function updateDots() {
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot, idx) => {
+		if (idx === index) {
+			dot.classList.add("dot_selected"); 
+		} else {
+			dot.classList.remove("dot_selected"); 
+		}
+	});
+}
